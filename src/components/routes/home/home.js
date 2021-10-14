3598c82e0835517bc117524244b38d959779144f
Danika H Jaskulak-Gonzalez <danikaherminise@gmail.com>
2021-10-14 15:10:02 -0700

    home page button
    got the button on the home page to link to the cards

--- src/components/routes/home/home.js
+++ src/components/routes/home/home.js
@@ -1,18 +1,25 @@
 import './home.css'
 
+import { NavLink, Switch, Route } from 'react-router-dom';
+
+import Cards from '../alphabet/cards';
+
 function Home () {
     return (
-        <div className="Banner">
-            <div className="text">
-                <h2>Welcome to Period Alphabet!</h2>
-                <p>Your survival guide for navigating your menstrual cycle. Connect with others, find resources near you, and discover tips that we wish we knew when growing up!</p>
-                <button type="submit">Get Started</button>
-            </div>
+        <Switch>
+            <div className="Banner">
+                <div className="text">
+                    <h2>Welcome to Period Alphabet!</h2>
+                    <p>Your survival guide for navigating your menstrual cycle. Connect with others, find resources near you, and discover tips that we wish we knew when growing up!</p>
+                    <NavLink to='/alphabet'><button type="submit">Get Started</button></NavLink>
+                </div>
 
-            <div className="image">
-                <img src="images/header.svg" alt="girl flexing"/>
+                <div className="image">
+                    <img src="images/header.svg" alt="girl flexing"/>
+                </div>
             </div>
-        </div>
+            <Route exact path='/alphabet' component={Cards}></Route>
+        </Switch>
     )
 }
 
