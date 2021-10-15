641315f0f0107c131f4f4789dc1413c85df2f824
Danika H Jaskulak-Gonzalez <danikaherminise@gmail.com>
2021-10-14 19:08:38 -0700

    some readme

---
+++ src/components/alphabet/cardRoutes/A.js
@@ -0,0 +1,9 @@
+function A() {
+    return (
+        <div className="A">
+            <h1>This is the page for Accessibility</h1>
+        </div>
+    )
+}
+
+export default A
--- src/components/alphabet/cards.js
+++ src/components/alphabet/cards.js
@@ -1,23 +1,32 @@
 import alphabet from './data.json'
 import './cards.css'
 
+import { NavLink, Switch, Route } from 'react-router-dom';
+
 function Cards () {
     return (
-        <div className="Cards">
-            <div className="banner">
-                <h1>ALPHABET</h1>
+        <Switch>
+            <div className="Cards">
+                <div className="banner">
+                    <h1>ALPHABET</h1>
+                </div>
+                <ul className="alphabet">
+                    {alphabet.map(l => 
+                        <NavLink to={`/${l.letter}`}>
+                            <li className="card">
+                                <img src={process.env.PUBLIC_URL + '/images/' + l.image} alt="card"/>
+                                <div className="word">
+                                    <h3>{l.title}</h3>
+                                </div>
+                            </li>
+                        </NavLink>
+                    )}
+                </ul>
             </div>
-            <ul className="alphabet">
-                {alphabet.map(letter => 
-                    <li className="card">
-                        <img src={process.env.PUBLIC_URL + '/images/' + letter.image} alt="card"/>
-                        <div className="word">
-                            <h3>{letter.title}</h3>
-                        </div>
-                    </li>
-                )}
-            </ul>
-        </div>
+            {alphabet.map(l => 
+                <Route exact path={`/${l.letter}`} component={eval(l.letter)}/>
+            )}
+        </Switch>
     );
 }
 
